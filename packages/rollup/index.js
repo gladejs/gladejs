0aa3d825d154db3fe84b47720d4bb5fefd20150a
Aurélien Stébé <aurelien.stebe@gmail.com>
2021-05-05 14:09:38 -0400

    feat(@rollup): add legacy plugin for IE 11 support

--- packages/rollup/index.js
+++ packages/rollup/index.js
@@ -1,11 +1,11 @@
 import { server } from './src/gladejs-server.js'
-import { browser } from './src/gladejs-browser.js'
-
+import { browser, legacy } from './src/gladejs-browser.js'
 import { serializer, moduleChunking } from './src/gladejs-utils.js'
 
 export default {
     server,
     browser,
+    legacy,
     serializer,
     moduleChunking,
 }
--- packages/rollup/src/gladejs-browser.js
+++ packages/rollup/src/gladejs-browser.js
@@ -4,8 +4,15 @@ import fs from 'fs-extra'
 import execa from 'execa'
 import bs from 'browser-sync'
 
+import { CSS_FILTER } from './gladejs-utils.js'
+
+let browserSync = false
+let legacyConfig = false
+
+const legacyInputId = '\0gladejs-legacy.js'
+
 export function browser(mainOutput, publicPath) {
-    let cssOutput, esmOutput, browserSync
+    let cssOutput, esmOutput
 
     return {
         name: 'gladejs/browser',
@@ -50,27 +57,79 @@ export function browser(mainOutput, publicPath) {
         },
 
         async closeBundle() {
-            const staticFile = path.resolve(mainOutput, 'static.mjs')
-
-            if (await fs.pathExists(staticFile)) {
-                if (!this.meta.watchMode) {
-                    const staticCode = '\n;(await run());process.exit(0);\n'
-                    await fs.appendFile(staticFile, staticCode)
-                }
-
-                await execa.node(staticFile)
-
-                if (this.meta.watchMode) {
-                    if (!browserSync) {
-                        browserSync = bs.create('gladejs')
-                        browserSync.init({ server: mainOutput })
-                    } else browserSync.reload()
-                } else await fs.remove(staticFile)
+            if (!legacyConfig) {
+                await staticServer(mainOutput, this.meta.watchMode)
             }
+        },
+
+        closeWatcher() {
+            if (!legacyConfig && browserSync) browserSync.exit()
+        },
+    }
+}
+
+export function legacy(mainOutput, publicPath) {
+    let legacyInputCode
+    legacyConfig = true
+
+    return {
+        name: 'gladejs/legacy',
+
+        options(options) {
+            legacyInputCode = Object.values(options.input).map(
+                (page) => `import '\0marko-browser-entry:${page}';`
+            )
+
+            options.input = legacyInputId
+        },
+
+        resolveId(id, importer) {
+            if (!importer && id === legacyInputId) return legacyInputId
+        },
+
+        load(id) {
+            if (id === legacyInputId) return legacyInputCode.join('\n')
+
+            if (CSS_FILTER.test(id)) return 'export default {};'
+        },
+
+        generateBundle(outputOptions, bundle) {
+            const output = outputOptions.dir.slice(mainOutput.length)
+
+            Object.values(bundle).forEach((legacy) => {
+                if (legacy.code.trim()) {
+                    legacy.type = 'asset'
+                    legacy.source = legacy.code
+                    legacy.filePath = `${publicPath}${output}/${legacy.fileName}`
+                } else delete bundle[legacy.fileName]
+            })
+        },
+
+        async closeBundle() {
+            await staticServer(mainOutput, this.meta.watchMode)
         },
 
         closeWatcher() {
             if (browserSync) browserSync.exit()
         },
     }
+}
+
+async function staticServer(output, isLive) {
+    const staticFile = path.resolve(output, 'static.mjs')
+    if (!(await fs.pathExists(staticFile))) return
+
+    if (!isLive) {
+        const staticCode = '\n;(await run());process.exit(0);\n'
+        await fs.appendFile(staticFile, staticCode)
+    }
+
+    await execa.node(staticFile)
+
+    if (isLive) {
+        if (!browserSync) {
+            browserSync = bs.create('gladejs')
+            browserSync.init({ server: output })
+        } else browserSync.reload()
+    } else await fs.remove(staticFile)
 }
